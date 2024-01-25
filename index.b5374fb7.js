62d1da15268d46a82fc5cc6180b93cf65998c79c
AlluringUnicorn <111394270+AlluringUnicorn@users.noreply.github.com>
2024-01-25 16:39:02 +0000

    Deploying to gh-pages from @ AlluringUnicorn/precision-dental-studio@2dfdce3a2935c6d4a02fea60e1f9458b6515cff9 🚀

--- index.b5374fb7.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("about_video");console.log(e);const o=document.querySelector(".play-btn");console.log(o);const t=document.querySelector(".play-svg");console.log(t),o.addEventListener("click",(function(){o.active?(e.pause(),o.active=!1,t.style.display="block"):(e.play(),o.active=!0,t.style.display="none")}));
-//# sourceMappingURL=index.b5374fb7.js.map
--- index.bdac7c40.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("about_video");console.log(e);var o=document.querySelector(".play-btn");console.log(o);var l=document.querySelector(".play-svg");console.log(l),o.addEventListener("click",(function(){o.active?(e.pause(),o.active=!1,l.style.display="block"):(e.play(),o.active=!0,l.style.display="none")}))}();
-//# sourceMappingURL=index.bdac7c40.js.map
