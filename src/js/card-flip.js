c08c0f3210599b11e110ad695588dedaaf641b4f
Mariia Khapun <mariakhapun@gmail.com>
2024-01-25 18:31:32 +0100

    update flip card

--- src/js/card-flip.js
+++ src/js/card-flip.js
@@ -1,9 +1,20 @@
 const cards = document.querySelectorAll('.doctors-card-wrapper');
 
-console.log(cards);
+const cardSVGs = document.querySelectorAll('.doctors-card-arrow');
 
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
+[...cards].forEach(card => {
+  card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
+
+    [...cardSVGs].find(svg => {
+    });
   });
 });
+
+
+
+
+
+ setTimeout(() => {
+        svg.classList.toggle('invisible');
+      }, 350);
